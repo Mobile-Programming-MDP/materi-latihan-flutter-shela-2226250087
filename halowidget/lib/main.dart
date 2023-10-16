af0d25b480209e5ff7238992b420130ecfff3ba3
shela-2226250087 <shela_2226250087@mhs.mdp.ac.id>
2023-10-16 19:04:31 +0700

    halowidget(widgetdemo)&wisata_candi

--- halowidget/lib/main.dart
+++ halowidget/lib/main.dart
@@ -10,36 +10,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Shela",
+        title: "Halo Widget",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: firstScreen());
+        home: WidgetDemo());
   }
 }
 
-class firstScreen extends StatelessWidget {
-  const firstScreen({super.key});
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Shela"),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ))
-        ],
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.home, color: Colors.white),
-        ),
-      ),
+          title: const Text("Shela"),
+          actions: <Widget>[
+            //icon di kanan appbar
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ))
+          ],
+          leading: IconButton(
+            //icon di kiri appbar
+            onPressed: () {},
+            icon: const Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          )),
       body: const Center(
         child: Text("Halo Shela"),
       ),
     );
+  }
+}
+
+ class WidgetDemo extends StatelessWidget{
+    const WidgetDemo({super.key});
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Widget Demo'),
+        ),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(16),
+            padding: const EdgeInsets.all(16),
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                'Ini adalah contoh Penggunaan container',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: (){}, 
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Tombol Elevated'),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.star, color: Colors.amber),
+              Text("Rating: 4.5"),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.network(
+              'https://picsum.photos/id/7/300/200',
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+   );
   }
 }
